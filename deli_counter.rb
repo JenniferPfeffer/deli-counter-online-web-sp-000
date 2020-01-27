010a3122519d8a2f69dd8997e630e2a72f02d7d1
Jennifer Pfeffer <jennifer.pfeffer127@gmail.com>
2020-01-27 15:07:39 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
 # Write your code here.
+def line(katz_deli) 
+  if katz_deli.length == 0 
+    puts "The line is currently empty." 
+  else katz_deli.each_with_index.map do |name,index|  
+    "The line is currenly #{index}. #{name}" 
+  end
+  end
+end 
+
+def take_a_number(katz_deli,name) 
+  
+  
